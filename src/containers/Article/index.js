bc3f0c3ba2f5f372d38fab5ee26b74528c7ae177
adeletang <lolied@DUNFRESH.LOCAL>
2021-08-04 16:02:50 +0200

    design page article

--- src/containers/Article/index.js
+++ src/containers/Article/index.js
@@ -13,24 +13,13 @@ const Article = () => {
     <Query query={ARTICLE_QUERY} slug={id}>
       {({ data: { articles } }) => {
         if (articles.length) {
-          const imageUrl =
-            process.env.NODE_ENV !== "development"
-              ? articles[0].image.url
-              : process.env.REACT_APP_BACKEND_URL + articles[0].image.url;
           return (
-            <div>
-              <div
-                id="banner"
-                className="uk-height-medium uk-flex uk-flex-center uk-flex-middle uk-background-cover uk-light uk-padding uk-margin"
-                data-src={imageUrl}
-                data-srcset={imageUrl}
-                data-uk-img
-              >
-              </div>
-              <div className="uk-section">
-                <div className="uk-container uk-container-small">
-                  <h1>{articles[0].title}</h1>
-                  <ReactMarkdown className="content">{articles[0].content}</ReactMarkdown>
+            <div className="contentArticle">
+              <img src={"http://localhost:1337"+articles[0].image.url} alt={articles[0].image.url} className="bannerArticle"/>
+              <div >
+                <div className="content">
+                  <h1 className="articleTitle">{articles[0].title}</h1>
+                  <ReactMarkdown >{articles[0].content}</ReactMarkdown>
                   <p className="author">
                     Ecrit par : {articles[0].author.name} le <Moment format="Do MMMM YYYY">{articles[0].published_at}</Moment>
                   </p>

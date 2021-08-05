56ce7e01fb39c2690319666b6767b14e9ebad769
adeletang <lolied@DUNFRESH.LOCAL>
2021-08-05 16:49:32 +0200

    debug des dossiers de galerie d'image + add du zoom sur les photos de la galerie

--- src/containers/Gallery/index.js
+++ src/containers/Gallery/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import GALLERY_QUERY from "../../queries/gallery/gallery.js";
 import Query from "../../components/Query";
 import { useParams } from "react-router";
+import Zoom from 'react-medium-image-zoom';
+
+import '../../assets/style/styleGallery.css';
 
 const Gallery = () => {
     let { id } = useParams();
@@ -9,11 +12,32 @@ const Gallery = () => {
         <div>
             <div className="content">
             <Query query={GALLERY_QUERY} slug={id}>
-                {({ data: { galleries } }) => { 
-                    return (
-                        <div>
-                            <h1>{galleries[0].name}</h1>
-                        </div>
+                {({ data: { galleries } }) => {
+                    /* console.log("-----------------5-") */
+                    return(<div>
+                        <h1>{galleries[0].name}</h1>
+                    {galleries.map(picture => {
+                        /* console.log(picture.photo.length) */
+                        const allPicture = picture.photo.slice(0, picture.photo.length);
+                        return (
+                            allPicture.map((photo, i) => {
+                                /* console.log(photo.url) */
+                                return (
+                                    <div>
+                                        <Zoom>
+                                        <img
+                                            src={"http://localhost:1337"+photo.url}
+                                            alt={photo.name}
+                                            className="test"
+                                        />
+                                        </Zoom>
+                                    </div>
+                                )
+                            })
+                        )
+                    
+                    })}
+                    </div>
                     )
                 }}
             </Query>
